1ab7d44f05b118309043c7d1926b597a9a13ae20
Josef Vacek <vacekj@outlook.com>
2022-04-13 19:37:25 +0200

    feat: cli interface, deleting

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use dialoguer::{theme::SimpleTheme, Confirm, MultiSelect};
 use fs_extra::dir::get_size;
 use human_bytes::human_bytes;
 use std::borrow::Cow;
+use std::fs::remove_dir_all;
 use std::ops::Deref;
 use std::os::unix::fs::MetadataExt;
 use std::path::Path;
+
 use walkdir::WalkDir;
 
 fn main() {
@@ -39,7 +42,37 @@ fn main() {
     node_modules.sort_unstable_by_key(|k| k.1);
     node_modules.reverse();
 
-    for dir in node_modules {
+    let count = node_modules.len();
+    let total_size = human_bytes(node_modules.iter().fold(0, |acc, x| acc + x.1) as f64);
+
+    println!("Found {count} node_module folders with a total size of {total_size}");
+    for dir in &node_modules {
         println!("{0} {1}", human_bytes(dir.1 as f64), dir.0);
+    }
+
+    let names: Vec<String> = node_modules.iter().map(|e| e.0.clone()).collect();
+
+    let selections = MultiSelect::with_theme(&SimpleTheme)
+        .with_prompt("Choose which node_modules to delete")
+        .items(&names)
+        .interact()
+        .unwrap();
+
+    if selections.is_empty() {
+        println!("No node_modules selected. Aborting...");
+        return;
+    } else {
+        println!("The following node_modules will be deleted:");
+        for selection in &selections {
+            println!("  {}", &names[*selection]);
+        }
+        if Confirm::new().with_prompt("Continue? ").interact().unwrap() {
+            for selection in &selections {
+                remove_dir_all(&names[*selection]);
+            }
+        } else {
+            println!("Aborting...");
+            return;
+        }
     }
 }
